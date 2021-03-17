74003f03e99e7f2081093985070c7248e00898c9
quarkusio <no-reply@quarkus.io>
2021-03-17 07:12:00 +0000

    Initial commit

---
+++ src/native-test/java/ru/tsc/t1/crm/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package ru.tsc.t1.crm;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
